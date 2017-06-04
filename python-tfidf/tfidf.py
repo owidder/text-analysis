e95a22f84d72c3bddbcf18afa1c6ac5ecb992ee2
Oliver Widder <oliver.widder@gmail.com>
2017-06-04 09:57:17 +0200

    refactor

--- python-tfidf/tfidf.py
+++ python-tfidf/tfidf.py
@@ -6,17 +6,6 @@ import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.stem.porter import PorterStemmer
 
-path = '/Users/owidder/dev/iteragit/nge/f2'
-token_dict = {}
-stemmer = PorterStemmer()
-
-def stem_tokens(tokens, stemmer):
-    stemmed = []
-    for item in tokens:
-        stemmed.append(stemmer.stem(item))
-    return stemmed
-
-
 def tokenize(text):
     tokens = nltk.word_tokenize(text)
     return tokens
@@ -37,26 +26,39 @@ def fileString(file_path):
     camel_case_split_no_single_chars = removeSingleChars(camel_case_split)
     return camel_case_split_no_single_chars
 
-for subdir, dirs, files in os.walk(path):
-    for file in files:
-        file_path = subdir + os.path.sep + file
-        if file_path.endswith("utf8"):
-            token_dict[file] = fileString(file_path)
+def tokenizePath(path):
+    token_dict = {}
+    for subdir, dirs, files in os.walk(path):
+        for file in files:
+            file_path = subdir + os.path.sep + file
+            if file_path.endswith("utf8"):
+                token_dict[file] = fileString(file_path)
+    return token_dict
 
-# this can take some time
-tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
-tfs = tfidf.fit_transform(token_dict.values())
+def fit(corpusPath):
+    token_dict = tokenizePath(corpusPath)
+    tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
+    tfidf.fit_transform(token_dict.values())
+    return tfidf
 
-test_str = fileString('/Users/owidder/dev/eos/inkassosystem.entwicklung/src/oci/InkassoServer/BuchungServer/BuchungServer.cc')
-print(test_str)
-test_resp = tfidf.transform([test_str])
+def findFeatures(corpusPath, rootPath):
+    tfidf = fit(corpusPath=corpusPath)
+    for subdir, dirs, files in os.walk(rootPath):
+        for file in files:
+            file_path = subdir + os.path.sep + file
+            if file_path.endswith("utf8"):
+                file_str = fileString(file_path)
+                file_response = tfidf.transform([file_str])
+                feature_names = tfidf.get_feature_names()
 
-feature_names = tfidf.get_feature_names()
-print(feature_names)
-f = {}
-for col in test_resp.nonzero()[1]:
-    f[feature_names[col]] = test_resp[0, col]
+                f = {}
+                for col in file_response.nonzero()[1]:
+                    f[feature_names[col]] = file_response[0, col]
 
-sf = sorted(f, key=f.__getitem__)
-for k in sf:
-    print(k + ": " + str(f[k]))
+                out_file = open(file_path + ".csv", 'w')
+                sf = sorted(f, key=f.__getitem__, reverse=True)
+                for k in sf:
+                    print(k + "\t" + str(f[k]), file=out_file)
+
+
+findFeatures(corpusPath='/Users/owidder/dev/iteragit/nge/f2', rootPath='/Users/owidder/dev/iteragit/nge/f2')
