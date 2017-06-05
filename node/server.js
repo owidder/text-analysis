cb142d3d68749c12d8234a67942d60a9087657ac
Oliver Widder <oliver.widder@gmail.com>
2017-06-05 13:24:10 +0200

    folder service

--- node/server.js
+++ node/server.js
@@ -3,36 +3,33 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var app = express();
+var fs = require('fs');
+
+var router = express.Router();
 
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 
-
-var latestRadarData;
+var BASE_FOLDER = '/Users/owidder/dev/iteragit/nge/python/f2-all';
 
 app.use(cors());
 app.use(bodyParser.json());
-
 app.use('/', express.static(__dirname + '/client'));
 
-server.listen(3002, function () {
-    console.log('Radar server listening on port 3002!')
+function readFolder(relFolder) {
+    var absFolder = BASE_FOLDER + '/' + relFolder;
+    return fs.readdirSync(absFolder);
+}
+
+router.get('/folder/*', function (req, res) {
+    var relFolder = req.originalUrl.substr("/api/folder".length+1);
+    res.json({
+        folder: relFolder,
+        content: readFolder(relFolder)
+    });
 });
 
+app.use('/api', router);
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.emit('initalItems', latestRadarData);
-
-    socket.on('itemsUpdate', function (items) {
-        console.log('received item update from client:');
-        console.log(items);
-        latestRadarData = items;
-        socket.broadcast.emit('refreshItems', items);
-    });
-
-    socket.on('reset', function() {
-        console.log('received reset request');
-        socket.broadcast.emit('reset');
-    })
+server.listen(3000, function () {
+    console.log('Radar server listening on port 3000!')
 });
