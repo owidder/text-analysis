7ab9d99c83127de631880117191b0a3af6f27a55
Oliver Widder <oliver.widder@gmail.com>
2017-06-12 20:56:23 +0200

    create matrix.csv

--- python/python-lsi/createMatrix.py
+++ python/python-lsi/createMatrix.py
@@ -11,10 +11,21 @@ with open ('./data/docnames', 'rb') as dn:
 lsi = models.LsiModel.load('./data/corpus.lsi')
 index = similarities.MatrixSimilarity.load('./data/corpus.100.index')
 
+out_file = open('./data/matrix.csv', 'w')
+
 for docname in docnames:
+    out_list = [docname]
     content = fileString(docname)
     vec_bow = dictionary.doc2bow(content.lower().split())
     vec_lsi = lsi[vec_bow]
     sims = index[vec_lsi]
-    for tuple in list(enumerate(sims)):
-        print(tuple)
+    ssims = sorted(enumerate(sims), key=lambda item: -item[1])
+
+    for tuple in list(enumerate(ssims))[:10]:
+        rel_docname = docnames[tuple[1][0]]
+        str_value = str(tuple[1][1])
+        if rel_docname != docname:
+            out_list.append(rel_docname)
+            out_list.append(str_value)
+
+    print("\t".join(out_list), file=out_file)
