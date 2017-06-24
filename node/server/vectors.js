5e23334098d70c734286e96e6872a75c60720548
Oliver Widder <oliver.widder@gmail.com>
2017-06-24 10:56:35 +0200

    vectors.js

--- node/server/vectors.js
+++ node/server/vectors.js
@@ -2,21 +2,46 @@
 
 var fs = require('fs');
 var _ = require('lodash');
+var math = require('math');
+
+var relPath = require('./relPath');
 
 var VECTORS_FILE_PATH = '../python/python-lsi/data/vectors.csv';
-var BASE_PATH = '/Users/owidder/dev/iteragit/nge/python/erpnext/';
 
 var vectors = {};
 
-function processVectorLine(line) {
+function extractVector(line) {
     var parts = line.split("\t");
-    var relPath;
+    parts.splice(0, 1);
+    var vector = parts.map(function (e) {
+        return Number(e);
+    });
+
+    return vector;
+}
+
+function extractRelPath(line) {
+    var parts = line.split("\t");
+    var relPath = relPath.makeRelPath(parts[0]);
+
+    return relPath;
+}
+
+function processVectorLine(line) {
+    var relPath = extractRelPath(line);
+    var vector = extractVector(line);
+
+    vectors[relPath] = vector;
 }
 
 function readVectors() {
-
+    var content = fs.readFileSync(VECTORS_FILE_PATH, 'utf8');
+    var lines = content.split("\n");
+    lines.forEach(function (line) {
+        processVectorLine(line);
+    });
 }
 
-function getVectorForFile(filePath) {
-
+function getVectorForFile(fileRelPath) {
+    return vectors[fileRelPath];
 }
