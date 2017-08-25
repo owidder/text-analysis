128f1759e8bebdcebd944c868b71979d53960f32
Oliver Widder <oliver.widder@gmail.com>
2017-08-25 09:12:13 +0200

    github: use commit id

--- node/client/js/util/github.js
+++ node/client/js/util/github.js
@@ -9,7 +9,7 @@ bottle.factory("github", function (container) {
             return "https://iteragit.iteratec.de/owd/nge-text-analysis/tree/master/python/OpenSpeedMonitor/" + relPath;
         }
         else if(relPath.startsWith("gitlab")) {
-            return "https://github.com/iteratec/OpenSpeedMonitor/blob/master/" + relPath.substr(7);
+            return "https://github.com/iteratec/OpenSpeedMonitor/blob/1df3a7ef8bd3a4ab7a54d97f313b5e2142390cd4/" + relPath.substr(7);
         }
 
         return "https://github.com/frappe/erpnext/blob/develop/" + relPath;
